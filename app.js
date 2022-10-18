b2f39988e8273b527ec891fe97d4c974499af8b4
Kirill <forworkskirill@gmail.com>
2022-10-19 00:36:42 +0500

    lauch mongo

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-
+// launch app
 const { PORT = 3000 } = process.env;
+// mongodb
+mongoose.connect('mongodb://localhost:27017/mestodb ');
 
 app.listen(PORT, () => {
-  console.log('Все успешно');
+  console.log(`приложение запущено на ${PORT} порту`);
 });
