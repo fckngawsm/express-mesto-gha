ad0e4ab55432c746d8e75896ab6106877e045e9b
Kirill <forworkskirill@gmail.com>
2022-11-05 23:15:53 +0500

    try5

--- utils/celebrate.js
+++ utils/celebrate.js
@@ -4,6 +4,9 @@ const { celebrate, Joi  } = require("celebrate");
 // create
 const celebrateSignin = celebrate({
   body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().required(),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
