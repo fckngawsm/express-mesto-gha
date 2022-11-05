fd1ac749f6248888aaa13ff6b68c078772cc3111
Kirill <forworkskirill@gmail.com>
2022-11-05 23:11:25 +0500

    middlewares with err log

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 const { loginUser, createUser } = require('./controlers/users');
 const { celebrateSignin, celebrateSignup } = require('./utils/celebrate');
-
+const {sendError} = require("./middlewares/sendErr")
 const app = express();
 // body-parser
 app.use(express.json());
@@ -31,6 +31,8 @@ app.post(
 app.use('/*', (req, res) => {
   res.status(404).send({ message: 'Ничего не нашлось' });
 });
+// middlewares with err
+app.use(sendError)
 // check npm run
 app.listen(PORT, () => {
   console.log(`приложение запущено на ${PORT} порту`);
