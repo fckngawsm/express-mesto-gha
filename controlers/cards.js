322f8df6a4ac612809ae46d9ff997efc667d86f3
Kirill <forworkskirill@gmail.com>
2022-11-06 21:23:05 +0500

    fixxx

--- controlers/cards.js
+++ controlers/cards.js
@@ -27,16 +27,20 @@ const postCards = (req, res, next) => {
 };
 // delete card by id
 const deleteCards = (req, res, next) => {
-  Cards.findByIdAndDelete(req.params.id)
-    .orFail(new Error("Not found"))
-    .then((card) => res.send({ data: card }))
-    .catch((card) => {
-      if (err.message === "Not found") {
-        next (new NotFound(`Нет карточки с id ${req.params.id}`));
-      }
+  Cards.findById(req.params.id)
+    .orFail()
+    .catch(() => {
+      throw new NotFound( 'Нет карточки с таким id' );
+    })
+    .then((card) => {
       if (card.owner.toString() !== req.user.id) {
-        next(new ForbiddenError("Недостаточно прав для выполнения операции"));
+        throw new ForbiddenError( 'Недостаточно прав для выполнения операции' );
       }
+      Cards.findByIdAndDelete(req.params.id)
+        .then((cardData) => {
+          res.send({ data: cardData });
+        })
+        .catch(next);
     })
     .catch(next);
 };
